d7c90f36fb1bf920435486a24417fabfbbfe41cf
Nooroong <sjiyeon759@gmail.com>
2026-02-24 10:37:39 +0900

    DTO와 엔티티를 스펙에 맞춰 수정

--- src/main/java/com/sprint/mission/discodeit/service/UserService.java
+++ src/main/java/com/sprint/mission/discodeit/service/UserService.java
@@ -1,23 +1,23 @@
 package com.sprint.mission.discodeit.service;
 
-import com.sprint.mission.discodeit.dto.UserDto;
 import com.sprint.mission.discodeit.dto.UserPatchDto;
 import com.sprint.mission.discodeit.dto.UserPostDto;
+import com.sprint.mission.discodeit.dto.UserResponseDto;
 import java.util.List;
 import java.util.UUID;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface UserService {
 
-  UserDto create(UserPostDto userPostDTO, MultipartFile profile);
+  UserResponseDto create(UserPostDto userPostDTO, MultipartFile profile);
 
-  UserDto findById(UUID userId);
+  UserResponseDto findById(UUID userId);
 
-  UserDto findByUserName(String userName); // 이름으로 조회(단건)
+  UserResponseDto findByUsername(String username); // 이름으로 조회(단건)
 
-  List<UserDto> findAll(); // 전체 조회(다건)
+  List<UserResponseDto> findAll(); // 전체 조회(다건)
 
-  UserDto updateUser(UUID userId,
+  UserResponseDto updateUser(UUID userId,
       UserPatchDto userPatchDTO, MultipartFile profile);
 
   void delete(UUID userId);
